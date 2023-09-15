f8cb4a1e66e989cde941649939216c026e6ae983
Tofunmi Akodu <tofunmi37@gmail.com>
2023-09-15 10:20:07 +0100

    first commit

--- tests/LoginScenarios.spec.js
+++ tests/LoginScenarios.spec.js
@@ -14,9 +14,7 @@ test("Valid Login", async ({ page }) => {
 
 test("Invalid Login Test", async ({ page }) => {
 
-
     await page.goto('https://www.saucedemo.com/');
-
     await page.locator('[data-test="username"]').fill('locked_out_user');
     await page.locator('[data-test="password"]').fill('secret_sauce');
     await page.locator('[data-test="login-button"]').click();
