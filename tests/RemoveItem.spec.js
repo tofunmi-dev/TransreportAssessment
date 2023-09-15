6233a2c5bb964112e276e66ea3e112c48738651b
Tofunmi Akodu <tofunmi37@gmail.com>
2023-09-15 10:15:33 +0100

    first commit

--- tests/RemoveItem.spec.js
+++ tests/RemoveItem.spec.js
@@ -7,6 +7,7 @@ test("Add and Remove items from cart", async ({ page }) => {
     await page.locator('[data-test="username"]').fill('standard_user');
     await page.locator('[data-test="password"]').fill('secret_sauce');
     await page.locator('[data-test="login-button"]').click();
+
     await page.locator('[data-test="product_sort_container"]').selectOption('lohi');
     await page.locator('[data-test="add-to-cart-sauce-labs-onesie"]').click();
     await page.locator('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click();
